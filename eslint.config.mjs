6caa034c8cedf6a44cc9268026859a43fd1d6ed7
tintinnkt <t.tintinnkt@gmail.com>
2025-02-23 16:11:33 +0700

    feat: initail project with base file and configure (eslint,prettier, js file)

---
+++ eslint.config.mjs
@@ -0,0 +1,102 @@
+import pluginJs from "@eslint/js";
+import globals from "globals";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      ecmaVersion: 2023,
+      globals: {
+        ...globals.node,
+        process: true,
+        __dirname: true,
+        require: true,
+      },
+    },
+    rules: {
+      //   Possible Problems
+      "no-unused-vars": [
+        "error",
+        {
+          vars: "all",
+          args: "after-used",
+          caughtErrors: "all",
+          ignoreRestSiblings: false,
+          reportUsedIgnorePattern: false,
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+      "no-undef": "warn",
+      "no-console": [
+        "warn",
+        {
+          allow: ["log", "error", "warn"],
+        },
+      ],
+
+      // Express.js Specific
+      "no-process-exit": "warn",
+      "handle-callback-err": ["error", "^(err|error)$"],
+
+      // Code Style
+      quotes: [
+        "warn",
+        "double",
+        {
+          avoidEscape: true,
+          allowTemplateLiterals: true,
+        },
+      ],
+      "space-before-function-paren": [
+        "error",
+        {
+          anonymous: "always",
+          named: "never",
+          asyncArrow: "always",
+        },
+      ],
+
+      // Variables & Functions
+      "no-var": "error",
+      "prefer-const": [
+        "error",
+        {
+          destructuring: "all",
+        },
+      ],
+      "no-unused-expressions": [
+        "error",
+        {
+          allowShortCircuit: true,
+          allowTernary: true,
+        },
+      ],
+
+      // Best Practices
+      curly: ["error", "multi-line"],
+      "dot-notation": "error",
+      eqeqeq: ["error", "always"],
+      "no-multi-spaces": "warn",
+      "no-multiple-empty-lines": [
+        "warn",
+        {
+          max: 2,
+          maxEOF: 1,
+        },
+      ],
+
+      // Modern JS Features
+      "object-shorthand": ["error", "always"],
+      "prefer-template": "error",
+      "template-curly-spacing": ["error", "never"],
+    },
+  },
+  pluginJs.configs.recommended,
+  {
+    ignores: ["node_modules/*", "build/*"],
+  },
+];
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const connectDB = require("./config/db");
+
+// Route files
+const auth = require("./routes/auth");
+const hospitals = require("./routes/hospitals");
+const appointments = require("./routes/appointments");
+
+// Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Cookie parser
+app.use(cookieParser());
+
+// Mount routers
+app.use("/api/v1/auth", auth);
+app.use("/api/v1/hospitals", hospitals);
+app.use("/api/v1/appointments", appointments);
+
+const PORT = process.env.PORT || 5003;
+const server = app.listen(
+  PORT,
+  console.log(
+    "Server running in ",
+    process.env.NODE_ENV,
+    " mode on port ",
+    PORT,
+  ),
+);
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  // Graceful shutdown
+  server.close(() => {
+    throw new Error("Server closed due to unhandled promise rejection");
+  });
+});
