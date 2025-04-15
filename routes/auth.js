727e077a19bbb7f33f1541f1e5b32d6e561300d9
ArthurEZ <thanagorn2005@gmail.com>
2025-04-15 15:40:56 +0700

    add route registerDentist

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,13 @@
 const express = require("express");
 
-const { register, login, logout, getMe, updateUser} = require("../controllers/auth");
+const { register, registerDentist, login, logout, getMe, updateUser} = require("../controllers/auth");
 
 const router = express.Router();
 
 const { protect, authorize } = require("../middleware/auth");
 
 router.post("/register", register);
+router.post("/registerDent", registerDentist);
 router.post("/login", login);
 router.get("/logout", protect, logout);
 router.get("/me", protect, getMe);
